2e19456120ae29bfdbeaed43951bc3cc14e27510
anas20023 <anasibnebelal400@gmail.com>
2026-05-06 22:38:22 +0600

    fix: optimize fetch call in getBooks function for improved performance

--- src/lib/books.js
+++ src/lib/books.js
@@ -1,24 +1,30 @@
 const bookapi=process.env.API_URL
 export const getBooks= async()=>{
     try {
-        const res = await (await fetch(`${bookapi}/books`)).json()
-        return res
+        const res = await fetch(`${bookapi}/books`,{
+            cache:'force-cache'
+        })
+        return res.json()
     } catch (error) {
         throw error
     }
 }
 export const getBooksbyID= async(bookID)=>{
     try {
-        const res = await (await fetch(`${bookapi}/books/${bookID}`)).json()
-        return res
+        const res =await fetch(`${bookapi}/books/${bookID}`,{
+            cache:'force-cache'
+        })
+        return res.json()
     } catch (error) {
         throw error
     }
 }
 export const getBooksbyCategory= async(category,id)=>{
     try {
-        const res = await (await fetch(`${bookapi}/books`)).json()
-        return res.filter((book=>(book.category===category && book.id !==Number(id)))).slice(0,4)
+        const res = await fetch(`${bookapi}/books`,{
+            cache:'force-cache'
+        })
+        return res.json().filter((book=>(book.category===category && book.id !==Number(id)))).slice(0,4)
     } catch (error) {
         throw error
     }
